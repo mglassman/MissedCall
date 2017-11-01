72ba1427e8934a6e698e033bd3bd012d0a671d2f
Michael Glassman <michael.glassman@gmail.com>
2017-11-01 15:46:15 -0700

    Better icon. Trying to get image to show for contact. Works on emulator.

--- app/src/main/java/com/example/micha/missedcall/IncomingCallReceiver.java
+++ app/src/main/java/com/example/micha/missedcall/IncomingCallReceiver.java
@@ -1,47 +1,54 @@
 package com.example.micha.missedcall;
 
-import android.app.Notification;
+
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
 import android.database.Cursor;
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.NotificationCompat;
 import android.telephony.TelephonyManager;
 
-/**
- * Created by micha on 10/28/2017.
- */
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class IncomingCallReceiver extends BroadcastReceiver {
 
-    static boolean ring = false;
-    static boolean callReceived = false;
-    static String callerPhoneNumber = "Unknown";
     static int notificationId = 0;
+    static String phoneNumber = "";
+    static Uri contactPhoto = null;
+    static boolean rang = false;
+    static boolean receivedCall = true;
 
-    public String getContactDisplayNameByNumber(String number, Context context) {
+
+    private String getContactDisplayNameByNumber(String number, Context context) {
+
+        String name = "Unknown caller";
+
         Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number));
-        String name = "Incoming call from";
 
         ContentResolver contentResolver = context.getContentResolver();
         Cursor contactLookup = contentResolver.query(uri, null, null, null, null);
 
+        contactPhoto = null;
         try {
             if (contactLookup != null && contactLookup.getCount() > 0) {
                 contactLookup.moveToNext();
                 name = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
-                // this.id =
-                // contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.CONTACT_ID));
-                // String contactId =
-                // contactLookup.getString(contactLookup.getColumnIndex(BaseColumns._ID));
-            } else {
-                name = "Unknown number";
+                contactPhoto = Uri.parse(contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.PHOTO_URI)));
             }
         } finally {
             if (contactLookup != null) {
@@ -52,18 +59,43 @@ public class IncomingCallReceiver extends BroadcastReceiver {
         return name;
     }
 
+
+
+    private Bitmap getContactBitmapFromURI(Context context, Uri uri) {
+        InputStream iStream = null;
+        Bitmap result = null;
+        try {
+            Uri displayPhotoUri = uri;
+            iStream = context.getContentResolver().openInputStream(displayPhotoUri);
+            result = BitmapFactory.decodeStream(iStream);
+            if (iStream != null) {
+                iStream.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result; // BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_contact);
+    }
+
+
+
     private void notify(String phoneNumber, Context mContext) {
 
         String name = getContactDisplayNameByNumber(phoneNumber, mContext);
-
 
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(mContext);
         mBuilder.setContentTitle("Oh shit!");
         mBuilder.setContentText(name + " " + phoneNumber);
         mBuilder.setSmallIcon(R.drawable.missed_call);
-        mBuilder.setPriority(Notification.PRIORITY_HIGH);
-        mBuilder.setColor(Color.BLACK);
+        mBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        if (contactPhoto != null) {
+            Bitmap bitmap = getContactBitmapFromURI(mContext, contactPhoto);
+            mBuilder.setLargeIcon(bitmap);
+        }
+
 
         NotificationManager mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -73,37 +105,37 @@ public class IncomingCallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context mContext, Intent intent) {
 
-
         // Get the current Phone State
         String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
 
-        if (state == null)
+        if (state == null) {
             return;
+        }
 
-        // If phone state "Ringing"
         if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-            ring = true;
-            callReceived = false;
-            // Get the Caller's Phone Number
+            rang = true;
+            receivedCall = false;
             Bundle bundle = intent.getExtras();
-            callerPhoneNumber = bundle.getString("incoming_number");
-        }
-
-        // If incoming call is received
-        if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-            callReceived = true;
-        }
-
-        // If phone is Idle
-        if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
-            // If phone was ringing(ring=true) and not received(callReceived=false) , then it is a missed call
-            if (ring && !callReceived) {
-                notify(callerPhoneNumber, mContext);
+            phoneNumber = bundle.getString("incoming_number");
+            if (phoneNumber == null) {
+                phoneNumber = "Blocked";
             }
-            ring = false;
-            callReceived = false;
         }
 
+
+        if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)){
+            receivedCall = true;
+        }
+
+
+        // If phone is Idle (probably just disconnected)
+        if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            if (rang && !receivedCall){
+                notify(phoneNumber, mContext);
+            }
+            rang = false;
+            receivedCall = false;
+        }
     }
 }
 
--- app/src/main/java/com/example/micha/missedcall/MainActivity.java
+++ app/src/main/java/com/example/micha/missedcall/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.micha.missedcall;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED) {
+//
+//            this.requestPermissions(new String[] {Manifest.permission.READ_CALL_LOG}, 1);
+//        }
+//
+//        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//
+//            this.requestPermissions(new String[] {Manifest.permission.READ_PHONE_STATE}, 2);
+//        }
+//
+//        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+//
+//            this.requestPermissions(new String[] {Manifest.permission.READ_CONTACTS}, 3);
+//        }
+
+        View view = findViewById(R.id.parent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (view != null) {
+                view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+
         setContentView(R.layout.activity_main);
     }
 }
